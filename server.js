7f54f9a7a4380eda5dd2029d2c68e5085ce9dcc7
aldike <aldy_mon@Mail.ru>
2023-04-05 15:27:04 +0600

    Previous lesson done

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) =>{
     res.render("index")
 })
 
+app.get('/login', (req, res)=>{
+    res.render("login")
+})
+
+app.get('/register', (req, res)=>{
+    res.render("register")
+})
+
 const PORT = 8000;
 
 app.listen(PORT, ()=>{
