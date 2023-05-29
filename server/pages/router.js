e2cbdb630a114353209781e51f39dbee380cd8a1
aldike <aldy_mon@Mail.ru>
2023-05-29 07:30:57 +0600

    Rates added

--- server/pages/router.js
+++ server/pages/router.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const Genres = require('../Genres/Genres');
 const Country = require('../Country/Country');
 const User = require('../auth/User');
-const Film = require('../Films/Film')
+const Film = require('../Films/Film');
+const Rate = require('../Rates/Rates')
 
 router.get('/', async(req, res) =>{
     const options = {}
@@ -92,7 +93,12 @@ router.get('/not-found', (req, res) =>{
 // })
 
 router.get('/detail/:id', async(req, res) =>{
+    const rates = await Rate.find({filmId: req.params.id}).populate('authorId')
+    let averageRate = 0;
+    for(let i = 0; i < rates.length; i++){
+        averageRate += rates[i].rate
+    }
     const film = await Film.findById(req.params.id).populate('country').populate('genre')
-    res.render("detail", {user: req.user ? req.user: {}, film: film})
+    res.render("detail", {user: req.user ? req.user: {}, film: film, rates: rates, averageRate: (averageRate / rates.length).toFixed(1)})
 })
 module.exports = router
